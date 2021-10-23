c31f7cbece0528793bf091cb64872807757acdf0
Bwardstories <thewardbunch@gmail.com>
2021-10-23 12:46:44 -0400

    started backend and added models and some routes

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    verified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
