7fe75d5a3b165ab6770ce17858e42bc0a4c496af
Bwardstories <thewardbunch@gmail.com>
2021-12-24 14:50:14 -0500

    finished mailchimp route

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
-const express = require("express");
-const authRouter = require("./auth");
-const eventRouter = require("./events");
+const express = require('express')
+const authRouter = require('./auth')
+const eventRouter = require('./events')
+const mailchimpRouter = require('./mailchimp')
 
-const router = express.Router();
+const router = express.Router()
 
-router.use("/auth", authRouter);
-router.use("/events", eventRouter);
-router.get("/", (req, res) => {
-  res.send("hello World");
-});
+router.use('/auth', authRouter)
+router.use('/events', eventRouter)
+router.use('/mailchimp', mailchimpRouter)
+router.get('/', (req, res) => {
+  res.send('hello World')
+})
 
-module.exports = router;
+module.exports = router
